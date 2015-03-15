62ee6e0bda66bed8a762699afb3df3d36bceb034
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-03-15 19:44:47 -0400

    move some things around

--- msd_site_settings.php
+++ msd_site_settings.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: MSD Site Settings
 Description: Provides settings panel for several social/address options and widgets/shortcodes/functions for display.
-Version: 0.4
+Version: 0.5
 Author: Catherine M OBrien Sandrick (CMOS)
 Author URI: http://msdlab.com/biological-assets/catherine-obrien-sandrick/
 GitHub Plugin URI: https://github.com/msdlab/msd_site_settings
@@ -15,7 +15,7 @@ if(!class_exists('GitHubPluginUpdater')){
 }
 
 if ( is_admin() ) {
-    new GitHubPluginUpdater( __FILE__, 'msd_lab', "msd_site_settings" );
+    new GitHubPluginUpdater( __FILE__, 'msdlab', "msd_site_settings" );
 }
 
 
@@ -31,10 +31,10 @@ class MSDSocial{
 		/*
 		 * Pull in some stuff from other files
 		 */
-		$this->requireDir($this->the_path . '/inc');
+		$this->requireDir($this->the_path . 'lib/inc');
         if(!is_admin()){
-    		wp_enqueue_style('msd-social-style',$this->the_url.'css/style.css');
-    		wp_enqueue_style('msd-social-style-'.$this->icon_size,$this->the_url.'css/style'.$this->icon_size.'.css');
+    		wp_enqueue_style('msd-social-style',$this->the_url.'lib/css/style.css');
+    		wp_enqueue_style('msd-social-style-'.$this->icon_size,$this->the_url.'lib/css/style'.$this->icon_size.'.css');
             wp_enqueue_style('font-awesome-style','//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css');
         }
         add_action('admin_enqueue_scripts', array(&$this,'add_admin_scripts') );
@@ -52,7 +52,7 @@ class MSDSocial{
             global $current_screen;
             if($current_screen->id == 'settings_page_msdsocial-options'){
                 wp_enqueue_script('bootstrap-jquery','//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js',array('jquery'));
-                wp_enqueue_script('timepicker-jquery',$this->the_url.'js/jquery.timepicker.min.js',array('jquery'));
+                wp_enqueue_script('timepicker-jquery',$this->the_url.'lib/js/jquery.timepicker.min.js',array('jquery'));
             }
         }
         
@@ -61,7 +61,7 @@ class MSDSocial{
             if($current_screen->id == 'settings_page_msdsocial-options'){
                 wp_register_style('bootstrap-style','//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css');
                 wp_register_style('font-awesome-style','//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css',array('bootstrap-style'));
-                wp_register_style('timepicker-style',$this->the_url.'css/jquery.timepicker.css');
+                wp_register_style('timepicker-style',$this->the_url.'lib/css/jquery.timepicker.css');
                 wp_enqueue_style('font-awesome-style');
                 wp_enqueue_style('timepicker-style');
             }
